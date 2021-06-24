23e9e58aba48ac0b8c0da36a26738fd13837e36b
zhzheng92 <billzhengzh@gmail.com>
2021-06-23 21:05:16 -0500

    Create TASSEL_GWAS_results_visualization.R

---
+++ scripts/TASSEL_GWAS_results_visualization.R
@@ -0,0 +1,19 @@
+setwd("~/AG2PI_GWAS_workshop/data/")
+library("qqman","tidyverse")
+
+# load GWAS output from TASSEL
+GWAS_output <- read_delim("mlm_output_tutorial4.txt",delim = "\t")
+
+# select trait "EarDia" (ear diameter) as an example for visualization
+gwasResults <- GWAS_output %>% filter(Trait == "EarDia" & Marker != "None") %>% 
+  dplyr::select(Marker,Chr,Pos,p) %>% 
+  dplyr::rename(SNP = Marker, CHR = Chr, BP = Pos, P = p) %>% 
+  mutate(qval = p.adjust(P, method = "BH"))
+
+# visualize results
+
+# manhattan plot
+manhattan(gwasResults, main = "Manhattan Plot", ylim = c(0, 10), cex = 0.6, cex.axis = 0.9, 
+          col = c("blue4", "orange3"), suggestiveline = F, genomewideline = -log10(0.05/2559), chrlabs = c(1:10))
+# qqplot
+qq(gwasResults$P)
